5a80e073f22fd51d1e05d5391ae552205682aede
Vijay Anand R <vanandr@cisco.com>
2015-12-09 09:32:40 +0530

    App code

---
+++ form-1/assets/js/placeholder.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	
+	$(".form-user-name").val("User name...");
+	$(".form-password").val("Password");
+	$(".form-domain-name").val("Domain name...");
+	$(".form-reg-ip").val("Registrar IP");
+	$(".form-udi").val("Device ID...");
+	
+});
